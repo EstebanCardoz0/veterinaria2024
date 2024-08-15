be54a1a645d93c9391ce4efdf03f492e582bfd95
esteban cardozo <esteban.cardozo.wec@gmail.com>
2024-08-15 14:46:09 -0300

    15 ago

--- src/main/java/org/example/veterinaria2024/controller/DuenioController.java
+++ src/main/java/org/example/veterinaria2024/controller/DuenioController.java
@@ -2,21 +2,32 @@ package org.example.veterinaria2024.controller;
 
 import org.example.veterinaria2024.Service.DuenioService;
 import org.example.veterinaria2024.Service.IDuenioService;
+import org.example.veterinaria2024.entity.Duenio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("/duenio")
 public class DuenioController {
 
     @Autowired
     IDuenioService duenioServi;
-    @Autowired private DuenioService duenioService;
 
-    @PostMapping ("/create")
-    public String createDuenio () {
-        duenioServi.createDuenio();
-        return "Duenio creado con exito";
+    @Autowired private DuenioService
+            duenioService;
+
+    @PostMapping("/create")
+    public String createDuenio(@RequestBody
+                               Duenio duen) {
+        duenioServi.createDuenio(duen);
+
+        return "Dueño creado con éxito";
+    }
+
+    @GetMapping("/get/{id}")
+    public Duenio getDuenio(
+            @PathVariable Long id) {
+
+        return duenioServi.getDuenio(id);
     }
 
 
