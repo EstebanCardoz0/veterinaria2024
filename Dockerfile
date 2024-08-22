bedd7b910eb20b1cc02e97f8f0b5ecb1a3cd0e41
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2024-08-21 21:29:32 -0300

    creacion docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17-jdk-slim
 LABEL authors="Esteban Cardozo"
-ARG JAR_FILE=target/Veterinaria2024-0.0.1
+ARG JAR_FILE=target/Veterinaria2024-0.0.1.jar
 COPY ${JAR_FILE} app_veterinaria.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","app_veterinaria.jar"]
--- src/main/java/org/example/veterinaria2024/Veterinaria2024Application.java
+++ src/main/java/org/example/veterinaria2024/Veterinaria2024Application.java
@@ -10,10 +10,10 @@ public class Veterinaria2024Application {
         SpringApplication.run(
                 Veterinaria2024Application.class,
                 args);
-        System.out.println("DB_URL: " + System.getenv("DB_URL"));
-        System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-        System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
-        System.out.println("DB_PLATFORM: " + System.getenv("DB_PLATFORM"));
+//        System.out.println("DB_URL: " + System.getenv("DB_URL"));
+//        System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+//        System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
+//        System.out.println("DB_PLATFORM: " + System.getenv("DB_PLATFORM"));
 
     }
 
